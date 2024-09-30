13c0da216731b232042194630b3d66e6773e5be7
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-09-30 19:40:56 +0100

    240930

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,19 @@
 const url = "http://127.0.0.1:5501/html/index.html";
+const iconsDiv = document.querySelector(".headerItens > div:nth-child(2");
 
 function showLoggedIcons() {
-    const iconsDiv = document.querySelector(".headerItens > div:nth-child(2");
-    
-    iconsDiv.innerHTML = 
-    `<i class="fa-solid fa-magnifying-glass" title="Pesquisa"></i>
-    <i class="fa-regular fa-user" title="Perfil"></i>
-    <i class="fa-solid fa-arrow-right-from-bracket" title="Logout"></i>`;
+    const buttons = iconsDiv.querySelectorAll("button"); 
+    buttons[1].innerHTML = `<i class="fa-regular fa-user" title="Perfil"></i>`;
+    buttons[2].innerHTML = `<i class="fa-solid fa-arrow-right-from-bracket" title="Logout"></i>`;
 }
 
+function showSearchInput() {
+    let searchIcon = document.querySelector("#searchIcon");
+    searchIcon.addEventListener("click", function () {
+        iconsDiv.innerHTML[0] += 
+        ``
+    })
 
+}
+showSearchInput();
 //showLoggedIcons();
