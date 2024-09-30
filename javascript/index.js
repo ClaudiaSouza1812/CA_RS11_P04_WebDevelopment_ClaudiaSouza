d4c1367cb024bed6cda7a6e46e8c368210a2c974
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-09-30 21:37:23 +0100

    240930

--- javascript/index.js
+++ javascript/index.js
@@ -7,13 +7,26 @@ function showLoggedIcons() {
     buttons[2].innerHTML = `<i class="fa-solid fa-arrow-right-from-bracket" title="Logout"></i>`;
 }
 
-function showSearchInput() {
+function showSearchTextInput() {
+    let form = iconsDiv.querySelector("form");
     let searchIcon = document.querySelector("#searchIcon");
-    searchIcon.addEventListener("click", function () {
-        iconsDiv.innerHTML[0] += 
-        ``
-    })
 
+    searchIcon.addEventListener("click", function () {
+
+        form.style.transition = "width 0.5s ease";
+
+        if (form.style.width === "0px" || form.style.width === "") {
+            form.style.display = "flex";
+            form.style.width = "300px";
+        } else {
+            form.style.width = "0px";
+            setDisplayTransition(() => {
+                form.style.display = "none";
+            }, 500);
+        }
+    });
 }
-showSearchInput();
+
+
+showSearchTextInput();
 //showLoggedIcons();
